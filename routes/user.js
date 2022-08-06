f7dcbbc0f2b255a95d281fa40fd5e260dcdb7d12
Rounak Jha <ronnie.jhaa@gmail.com>
2022-08-06 18:37:07 +0545

    Basic app

---
+++ routes/user.js
@@ -0,0 +1,33 @@
+let patientModel = require('../model/patientModel');
+
+
+let path = require('path');
+let router = require('express').Router();
+router.get('/',async (req,res)=>{
+    console.log("Do we reach the user page");
+    let result = await patientModel.find({}).sort({
+        "problem.percentage":-1
+    }).limit(10);
+    console.log(result);
+    let renderData =result.map((data)=>{
+        return {
+            name:data.name,
+            age:data.age,
+            gender:data.gender,
+            disL:data.leftEyeProblem.name,
+            perL:data.leftEyeProblem.percentage.toFixed(2),
+            disR:data.rightEyeProblem.name,
+            perR:data.rightEyeProblem.percentage.toFixed(2)
+        }
+    })
+    res.render('profilePage/index',{data:renderData})
+})
+
+router.post('/',async (req,res)=>{
+    console.log("We are good");
+    let result = await patientModel.find({}).sort({age:-1}).limit(3);
+    res.status(200).send(result)
+    console.log(result)
+})
+
+module.exports = router;
